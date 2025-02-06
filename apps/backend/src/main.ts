d835abfb165564e5c214a0b7cf9add5efb49c73c
Emil Litwiniec <elitwiniec@apptension.com>
2025-02-06 12:59:46 +0100

    feat: Add base FE and BE

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,21 +1,33 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import express from 'express';
-import * as path from 'path';
+import { WebSocketServer } from 'ws';
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+const port = process.env.PORT || 3001;
+const wsPort = process.env.WS_PORT || 8080;
+
+const ws = new WebSocketServer({ port: +wsPort });
+
+ws.on('listening', () => {
+  console.log(`WebSocketServer on port ${wsPort} ${JSON.stringify(ws)}`);
+});
+
+ws.on('connection', (ws) => {
+  ws.on('error', (error) => {
+    console.error(error);
+  });
+
+  ws.on('message', (data) => {
+    console.warn('DEBUGPRINT[43]: main.ts:20: data=', data);
+  });
+});
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to PONG server!' });
 });
 
-const port = process.env.PORT || 3001;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
+
 server.on('error', console.error);
